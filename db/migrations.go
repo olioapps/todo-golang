4225eb5184b772b30dbce4b798b74e5c492a8a12
Jason Lane <quizathaderat@yahoo.com>
2017-03-23 12:02:02 -0700

    Add todo list

--- db/migrations.go
+++ db/migrations.go
@@ -17,6 +17,8 @@ func NewMigrationsContainer(connectionManager *olioDAO.ConnectionManager) Migrat
 func (m MigrationsContainer) GetMigrations() []olioDb.Migration {
 	var migrations []olioDb.Migration = []olioDb.Migration{
 		m.v1(),
+		m.v2(),
+		m.v3(),
 	}
 	return migrations
 }
@@ -36,6 +38,44 @@ func (m MigrationsContainer) v1() olioDb.Migration {
 				PRIMARY KEY (id)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
         `).Error; err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func (m MigrationsContainer) v2() olioDb.Migration {
+	return func() error {
+		db := m.connectionManager.GetDb()
+
+		log.Info("Create table todo list")
+
+		if err := db.Exec(`
+		    CREATE TABLE IF NOT EXISTS
+			todo_lists (
+				id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+				name varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+                created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+				updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+				PRIMARY KEY (id)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+		`).Error; err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func (m MigrationsContainer) v3() olioDb.Migration {
+	return func() error {
+		db := m.connectionManager.GetDb()
+
+		log.Info("Add todo list foreign key")
+
+		if err := db.Exec(`
+		     	ALTER TABLE todo_items ADD COLUMN todo_list_id bigint(20) unsigned,
+				ADD FOREIGN KEY (todo_list_id) REFERENCES todo_lists(id)
+		`).Error; err != nil {
 			return err
 		}
 		return nil
